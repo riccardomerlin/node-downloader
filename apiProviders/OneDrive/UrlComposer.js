2d059963ae49c040d0a1ecd9d80b80a3c7c4eba0
Riccardo Merlin <riccardomerlin@gmail.com>
2018-03-02 18:44:30 +0000

    fix url composer

--- apiProviders/OneDrive/UrlComposer.js
+++ apiProviders/OneDrive/UrlComposer.js
@@ -3,7 +3,7 @@ const urlJoin = require('proper-url-join');
 class UrlComposer {
   static compose(baseUri, path) {
     return `${typeof path === 'undefined' || path.trim() === ''
-      ? urlJoin(baseUri)
+      ? `${urlJoin(baseUri)}`
       : `${urlJoin(baseUri)}:${urlJoin(path)}:`}`;
   }
 }
